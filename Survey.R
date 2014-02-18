c8fd8e1536c71b0249e3f6055dfd2dc98631de53
Dublin R <DublinRUserGroup@gmail.com>
2014-02-18 16:51:17 +0000

    Create Survey.R

---
+++ Survey.R
@@ -0,0 +1,8 @@
+
+download.file("http://stat.duke.edu/courses/Fall11/sta101.02/labs/survey.RData", 
+     destfile = "survey.RData")
+
+
+# gender  - What is your gender?
+# siblings - How many siblings do you have?
+# birthOrder - What is your birth order? (1 for oldest/only child, 2 for second oldest, etc.)
