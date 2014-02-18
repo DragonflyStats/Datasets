c103291f6813291dc1d6a5b382a6924c6fd3e197
Dublin R <DublinRUserGroup@gmail.com>
2014-02-18 16:38:56 +0000

    Create DASI-wk1quiz.R

---
+++ DASI-wk1quiz.R
@@ -0,0 +1,7 @@
+plot(present$year, present$boys, type="l",col="red", 
+     ylab="numbers",xlab="Year")
+points(present$year, present$girls, type="l",col="blue")
+
+#--------------------------------------------#
+
+summary(present)
