a955086de77c80fe05fb1aaaff5555ab539cc83e
DragonflyStats <kobriendublin@gmail.com>
2015-10-17 00:32:16 +0100

    Reload

--- OUT/DASI-wk1quiz.R
+++
@@ -1,7 +0,0 @@
-plot(present$year, present$boys, type="l",col="red", 
-     ylab="numbers",xlab="Year")
-points(present$year, present$girls, type="l",col="blue")
-
-#--------------------------------------------#
-
-summary(present)
